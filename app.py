fef91bb3766718204b1320f66d96470047a86e4c
DmitriyPershukov <dmitriypershukov@gmail.com>
2024-05-22 18:01:37 +0500

    fixed spelling; implemented choosing customer by name in streamlit app

--- app.py
+++ app.py
@@ -6,13 +6,14 @@ st.header("Рекомендательная система с использов
 
 model = ALSRecommender("./AlternatingLeastSquares/Model")
 
-user_for_prediction = st.selectbox("Выберите пользователя для рекомендации товаров", model.customer_id)
+user_for_prediction = st.selectbox("Выберите пользователя для рекомендации товаров", model.customers['customer_name'].values)
 
 if st.button("Получить рекомедации"):
-    recommendations = model.get_product_name_recommendations(user_for_prediction)
+    recommendations = model.get_recommendations_with_names(user_for_prediction)
     result = ""
 
     dataframe_data = {"Рекомендации": recommendations[0], "Оценка Рекомендации": recommendations[1]}
     dataframe = pandas.DataFrame(dataframe_data)
+    dataframe = dataframe[dataframe["Оценка Рекомендации"] > 0.00001]
 
     st.dataframe(dataframe, hide_index= True)
