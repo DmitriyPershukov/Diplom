21200d039c6a1620499bf6163a08747b4a53275c
DmitriyPershukov <dmitriypershukov@gmail.com>
2024-06-05 20:25:19 +0500

    fixed recommendation dataframe processing in streamlit app for altALS

--- app.py
+++ app.py
@@ -11,11 +11,16 @@ user_for_prediction = st.selectbox("Выберите пользователя д
 
 if st.button("Получить рекомедации"):
     recommendations = model.get_recommendations_with_names(user_for_prediction)
+    result = ""
+
+    dataframe_data = {"Рекомендации": recommendations.product_name, "Оценка Рекомендации": recommendations.score}
+    dataframe = pandas.DataFrame(dataframe_data)
+    dataframe = dataframe[dataframe["Оценка Рекомендации"] > 0.00001]
 
     max_score = None
-    for index, row in recommendations.iterrows():
+    for index, row in dataframe.iterrows():
         if max_score is None:
             max_score = row["Оценка Рекомендации"]
-        recommendations.at[index, "Оценка Рекомендации"] = 100 / (max_score / row["Оценка Рекомендации"])
+        dataframe.at[index, "Оценка Рекомендации"] = 100 / (max_score / row["Оценка Рекомендации"])
 
-    st.dataframe(recommendations, hide_index= True)
+    st.dataframe(dataframe.iloc[:7], hide_index= True)
